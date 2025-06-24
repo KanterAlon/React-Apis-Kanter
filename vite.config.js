8cdf0c9e43e4116b934b287bd298a1c55c877867
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2025-06-24 08:38:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: '/React-Apis-Kanter/',
-  plugins: [react()],
+  plugins: [react()]
 })
